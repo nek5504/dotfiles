e26059acceba593d21b85d371d4c0f8c7288658d
My Name <me@example.com>
2017-08-17 13:48:22 +0900

    * Add install/backup script.

---
+++ runme.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+backup() {
+  for f in $(ls -a ./dotfiles/)
+  do
+    if [ "." = "$f" -o ".." = "$f" ]; then
+      continue
+    fi
+
+    echo "Copy: \"${HOME}/$f\" -> \"./dotfiles/$f\""
+    cp -a "${HOME}/$f" "./dotfiles/$f"
+  done
+}
+
+install() {
+  for f in $(ls -a ./dotfiles/)
+  do
+    if [ "." = "$f" -o ".." = "$f" ]; then
+      continue
+    fi
+
+    echo "Copy: \"./dotfiles/$f\" -> \"${HOME}/$f\""
+    cp -a "./dotfiles/$f" "${HOME}/$f"
+  done
+}
+
+help() {
+cat << EOF >&2
+Usage: $(basename $0) [command]
+
+command:
+  -b, --backup      Backup local dotfiles.
+  -i, --install	    Install dotfiles.
+EOF
+}
+
+set -e
+
+case $1 in
+  -b|--backup) backup ;;
+  -i|--install) install ;;
+  *) help ;;
+esac
